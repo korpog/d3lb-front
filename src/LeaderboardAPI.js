daaed7388bf403ddb6903f9e144152b820f959be
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-11-25 19:03:47 +0100

    fixed API url

--- src/LeaderboardAPI.js
+++ src/LeaderboardAPI.js
@@ -5,12 +5,12 @@ const API_URL2 = 'https://d3lb.herokuapp.com';
 export default class LeaderboardAPI{
 
     getLeaderboards() {
-        const url = `${API_URL}/leaderboards/`;
+        const url = `${API_URL2}/leaderboards/`;
         return axios.get(url).then(response => response.data);
     }  
     
     getLeaderboard(slug) {
-        const url = `${API_URL}/leaderboards/${slug}`;
+        const url = `${API_URL2}/leaderboards/${slug}`;
         return axios.get(url).then(response => response.data);
     }
 }
